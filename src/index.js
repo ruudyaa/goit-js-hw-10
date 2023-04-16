0d54ec425ad3312c7e0261d5cd0559c30dbb07ea
ruudyaa <martarudnytska26@gmail.com>
2023-04-16 21:57:28 +0300

    Adds js code for main task.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+const searchBox = document.querySelector('#search-box');
+const body = document.querySelector('body');
+
+countryList.style.visibility = 'hidden';
+countryInfo.style.visibility = 'hidden';
+
+searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(e) {
+  e.preventDefault();
+
+  const searchCountries = e.target.value.trim();
+
+  if (!searchCountries) {
+    countryList.style.visibility = 'hidden';
+    countryInfo.style.visibility = 'hidden';
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(searchCountries)
+    .then(result => {
+      if (result.length > 10) {
+        Notify.info(
+          'Too many matches found. Please, enter a more specific name.'
+        );
+        return;
+      }
+      renderedCountries(result);
+    })
+    .catch(error => {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function renderedCountries(result) {
+  const inputLetters = result.length;
+
+  if (inputLetters === 1) {
+    countryList.innerHTML = '';
+    countryList.style.visibility = 'hidden';
+    countryInfo.style.visibility = 'visible';
+    countryCardMarkup(result);
+  }
+
+  if (inputLetters > 1 && inputLetters <= 10) {
+    countryInfo.innerHTML = '';
+    countryInfo.style.visibility = 'hidden';
+    countryList.style.visibility = 'visible';
+    countriesListMarkup(result);
+  }
+}
+
+function countriesListMarkup(result) {
+  const listMarkup = result
+    .map(({ name, flags }) => {
+      return `
+      <li class="list-item"><img class="item-img" src="${flags.svg}" alt="${name}" width="60" height="auto"><span>${name.official}</span></li>`;
+    })
+    .join('');
+  countryList.innerHTML = listMarkup;
+  return listMarkup;
+}
+
+function countryCardMarkup(result) {
+  const cardMarkup = result
+    .map(({ flags, name, capital, population, languages }) => {
+      languages = Object.values(languages).join(', ');
+      return `
+            <img src="${flags.svg}" alt="${name}" width="320" height="auto">
+            <p> ${name.official}</p>
+            <p>Capital: <span> ${capital}</span></p>
+            <p>Population: <span> ${population}</span></p>
+            <p>Languages: <span> ${languages}</span></p>`;
+    })
+    .join('');
+  countryInfo.innerHTML = cardMarkup;
+  return cardMarkup;
+}
