c77c41bbe2d600f7301ba0660b32b997dddb9d40
ruudyaa <martarudnytska26@gmail.com>
2023-04-16 21:57:47 +0300

    Adds js code of a separate function

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+const url = 'https://restcountries.com/v3.1/name/';
+const searchParams = new URLSearchParams({
+  fields: ['name', 'capital', 'population', 'flags', 'languages'],
+});
+
+export const fetchCountries = name => {
+  return fetch(`${url}${name}?${searchParams}`).then(response => {
+    if (response.status === 404) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
